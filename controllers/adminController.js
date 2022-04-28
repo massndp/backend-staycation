429c4d8e3afe5817cea15704eb69abc5c0a8b713
massndp <bayu8989.88@gmail.com>
2022-04-28 14:00:11 +0700

    view item

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -7,5 +7,8 @@ module.exports = {
     },
     viewBank: (req, res) => {
         res.render('admin/bank/view_bank');
+    },
+    viewItem: (req, res) => {
+        res.render('admin/item/view_item');
     }
 }
